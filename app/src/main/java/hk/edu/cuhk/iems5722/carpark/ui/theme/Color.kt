08f295e665f47883152611c9a71b0def96e7a859
Alwyn-Tan <2660412180@qq.com>
2025-11-25 23:17:32 +0800

    Initial commit

---
+++ app/src/main/java/hk/edu/cuhk/iems5722/carpark/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package hk.edu.cuhk.iems5722.carpark.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LightBlue = Color(0xFFE3F2FD)
+
+val White = Color(0xFFFFFFFF)
+
+val DarkBlue = Color(0xFF2196F3)
+
+val DarkBlueBackground = Color(0xFF0D47A1)
---
+++ app/src/main/java/hk/edu/cuhk/iems5722/carpark/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package hk.edu.cuhk.iems5722.carpark.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = DarkBlueBackground,
+    secondary = LightBlue,
+    tertiary = White
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = DarkBlue,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
+
+@Composable
+fun CarparkTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
