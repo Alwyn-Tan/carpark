4f35bb7f6764dc30a2883ad35f274f110ed77c29
Alwyn-Tan <2660412180@qq.com>
2025-11-26 19:00:52 +0800

    fix: getVacancyByParkId()

--- app/src/main/java/hk/edu/cuhk/iems5722/carpark/data/CarParkRepository.kt
+++ app/src/main/java/hk/edu/cuhk/iems5722/carpark/data/CarParkRepository.kt
@@ -6,29 +6,20 @@ import hk.edu.cuhk.iems5722.carpark.network.CarParkApiService
 
 interface CarParkRepository {
     suspend fun getCarParks(): List<CarParkBasicInfo>
-    
-    suspend fun getVacancies(): List<CarParkVacancy>
-    
+
     suspend fun getVacancyByParkId(parkId: String): CarParkVacancy?
 }
 
-class NetworkCarParkRepository(
-    private val carParkApiService: CarParkApiService
-) : CarParkRepository {
-    
+class NetworkCarParkRepository(private val carParkApiService: CarParkApiService) :
+        CarParkRepository {
+
     override suspend fun getCarParks(): List<CarParkBasicInfo> {
         val response = carParkApiService.getCarParksBasicInfo()
         return response.carParks
     }
-    
-    override suspend fun getVacancies(): List<CarParkVacancy> {
-        val response = carParkApiService.getCarParksVacancy()
-        return response.carParks
-    }
-    
+
     override suspend fun getVacancyByParkId(parkId: String): CarParkVacancy? {
-        val response = carParkApiService.getCarParksVacancy()
-        return response.carParks.find { it.parkId == parkId }
+        val response = carParkApiService.getCarParkVacancyByParkId(parkId)
+        return response.carParks.firstOrNull()
     }
 }
-
--- app/src/main/java/hk/edu/cuhk/iems5722/carpark/network/CarParkApiService.kt
+++ app/src/main/java/hk/edu/cuhk/iems5722/carpark/network/CarParkApiService.kt
@@ -3,13 +3,12 @@ package hk.edu.cuhk.iems5722.carpark.network
 import hk.edu.cuhk.iems5722.carpark.model.CarParkBasicInfoResponse
 import hk.edu.cuhk.iems5722.carpark.model.CarParkVacancyResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CarParkApiService {
-    
-    @GET("basic_info_all.json")
-    suspend fun getCarParksBasicInfo(): CarParkBasicInfoResponse
-    
-    @GET("vacancy_all.json")
-    suspend fun getCarParksVacancy(): CarParkVacancyResponse
-}
 
+    @GET("basic_info_all.json") suspend fun getCarParksBasicInfo(): CarParkBasicInfoResponse
+
+    @GET("vacancy_{parkId}.json")
+    suspend fun getCarParkVacancyByParkId(@Path("parkId") parkId: String): CarParkVacancyResponse
+}
